c08d918aa31f6738c3cacb81ab88f0cf1891669a
dla216 <danny_liu_5@sfu.ca>
2023-03-02 15:27:54 -0800

    add reducer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,8 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { StoreModule } from '@ngrx/store';
 
+import { counterReducer } from './counter.reducer';
+
 @NgModule({
   declarations: [
     AppComponent
